abc318d5b628011d24987fd59d2da999c2b365e4
Giovanne Piçuti <picuti256@gmail.com>
2022-12-15 09:31:50 -0300

    Aula de procotolo Http e integração da API

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,6 +3,30 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url).then((response) => {
-    console.log(response)
-})
+const convertPokemonToList = (pokemon) => {
+    return `<li class="pokemon">
+    <span class="pokeNumber">#001</span>
+    <span class="pokeName">${pokemon.name}</span>
+
+    <div class="pokeInfo">
+        <ol class="pokeTypes">
+            <li class="type">Grass</li>
+            <li class="type">Poison</li>
+        </ol>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="${pokemon.name}" />
+    </div>
+</li>`
+}
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemonList) => {
+        for (let i = 0; i < pokemonList.length; i++) {
+            const pokemon = pokemonList[i];
+            console.log(convertPokemonToList(pokemon))
+
+        }
+    })
+    .catch((err) => console.error(err))
+    .finally(() => console.log('Requisição concluída'))
