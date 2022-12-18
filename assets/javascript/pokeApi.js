59d18960034d00b82a28a3707b5b9b90b2dce155
Giovanne Piçuti <picuti256@gmail.com>
2022-12-18 19:13:45 -0300

    Pequenos ajustes no chamada da API

--- assets/javascript/pokeApi.js
+++ assets/javascript/pokeApi.js
@@ -1,10 +1,22 @@
 const pokeApi = {}
 
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map((pokemon) => fetch(pokemon.url).then((response) => response.json())))
+        .then((detailRequest) => Promise.all(detailRequest))
+        .then((pokemonsDetails) => { console.log(pokemonsDetails) })
         .catch((err) => console.error(err));
 }
+
+Promise.all([
+    fetch('https://pokeapi.co/api/v2/pokemon/1'),
+    fetch('https://pokeapi.co/api/v2/pokemon/2'),
+    fetch('https://pokeapi.co/api/v2/pokemon/3'),
+    fetch('https://pokeapi.co/api/v2/pokemon/4')
+]).then((results) => {
+    console.log(results)
+})
